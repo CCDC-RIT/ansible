4d43131311379d55e39303f1a3950a78211cfd94
Drew Young <dmy625@comcast.net>
2024-01-18 20:08:35 -0500

    added install script

---
+++ install_req.sh
@@ -0,0 +1,14 @@
+# install requirements to run ansible
+
+# ensure root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# install required packages
+apt-get update
+apt-get install -y python-pip python-dev libffi-dev libssl-dev git sshpass ansible 
+
+# install required python modules
+pip install pywinrm 
